443303285306b2f224456d1a5e5127c1e0476beb
Kiura Magomadov <m.k.x@mail.ru>
2021-03-20 20:39:37 +0300

    added localstorage

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
-let CHECKLIST = [
-    
-    
-      
-  { name: "Arav", checked: false },
-  { name: "Is", checked: true },
-  { name: "Nice", checked: false }
-  
-    
-];
 
+function getItem(key) {
+  return JSON.parse(localStorage.getItem(key))
+}
 
+function setItem(key, item) {
+  localStorage.setItem(key, JSON.stringify(item))
+}
+
+function init_list() {
+  let checklist = getItem('checklist')
+  if (!checklist || checklist.length === 0) {
+    checklist = [
+      { name: "meet.new", checked: false },
+      { name: "webex.new", checked: true },
+      { name: "teams.new", checked: false }
+    ]
+  }
+  setItem('checklist', checklist)
+}
 
 function render_list() {
   console.log(" `render_list` ran");
-  const shopping_list_string = generate_list_string(CHECKLIST);
+  const shopping_list_string = generate_list_string();
   $(".js-shopping-list").html(shopping_list_string);
 }
 var show = ""
-function generate_list_string(shopping_list) {
+function generate_list_string() {
   console.log("Genrating Shopping List Elenet");
-  
+
   var listAsString = "";
 
-  listAsString
+  var shopping_list = getItem('checklist');
+  if (!shopping_list) return listAsString;
 
+  for (var i = 0; i < shopping_list.length; i++) {
 
-  for( var i = 0; i  < shopping_list.length; i++){
-   
-      /*send items to generate_item_element and add it to listAsString*/
+    /*send items to generate_item_element and add it to listAsString*/
     listAsString += generate_item_element(shopping_list[i])
-  
-   
+
+
   }
   return listAsString
 }
@@ -38,22 +46,31 @@ function generate_list_string(shopping_list) {
 
 
 
-  function add_item_to_list(itemName) {
+function add_item_to_list(itemName) {
   console.log(`Adding "${itemName}" to shopping list`);
 
+  var checklist = getItem('checklist');
+  if (!checklist) return;
 
-   CHECKLIST.push( { name: itemName, checked: false } )
-   alert("str" + itemName)
-
-  //add an item to the CHECKLIST
+  checklist.push({ name: itemName, checked: false });
+  setItem('checklist', checklist);
+  alert("str" + itemName);
 }
-var prompt = prompt("what is the name")
+
+let firstName = getItem('firstName');
+if (!firstName) {
+  firstName = prompt("what is the name");
+  if (firstName) {
+    setItem('firstName', firstName);
+  }
+}
+
 function handle_add_item() {
-  $("#clickBtn").on("click",function (event) {
+  $("#clickBtn").on("click", function (event) {
     event.preventDefault();
     console.log("`handle_add_item` ran");
-    
-  var str = $("#task").val();
+
+    var str = $("#task").val();
 
     add_item_to_list(str)
     render_list()
@@ -61,11 +78,12 @@ function handle_add_item() {
 }
 
 
-function handle_check_item() {}
+function handle_check_item() { }
 
-function handle_delete_item() {}
+function handle_delete_item() { }
 
 function handle_shopping_list() {
+  init_list();
   render_list();
   handle_add_item();
   handle_check_item();
@@ -77,10 +95,10 @@ function generate_item_element(item) {
     
   
   <li data-item-id="${item.name}">
-      <a class="shopping-item js-shopping-item href= " ${item.checked}? "  shopping-item__checked" : ''}">${prompt}</span>
+      <a class="shopping-item js-shopping-item href="${item.checked}?"  shopping-item__checked" : ''}">${firstName}</span>
       <div class="shopping-item-controls">
-      <a href="${item.name}"> ${item.name} </a>
-          <button class="shopping-item-toggle js-item-toggle"><span class="button-label"><a href="Join"> Join</span></button>
+      <p> ${item.name} </p>
+          <button class="shopping-item-toggle js-item-toggle"><span class="button-label"><a href="https://${item.name}"> Join</span></button>
           <button class="shopping-item-delete js-item-delete"><span class="button-label" >Delete</span> <a href="${item.name}"> <a></button>
       </div>
   </li>`;
